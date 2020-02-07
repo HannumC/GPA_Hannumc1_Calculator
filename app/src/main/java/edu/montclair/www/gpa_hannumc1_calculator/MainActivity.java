631d2b2c3ee6d2d29d0e551a91ccdfd2789413bd
coreyhannum <hannumc1@montclair.edu>
2020-02-07 17:34:02 -0500

    new edits

--- app/src/main/java/edu/montclair/www/gpa_hannumc1_calculator/MainActivity.java
+++ app/src/main/java/edu/montclair/www/gpa_hannumc1_calculator/MainActivity.java
@@ -2,8 +2,11 @@ package edu.montclair.www.gpa_hannumc1_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,30 +15,113 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView dspGPA = findViewById(R.id.DispGPA);
+        final Button calcGPA = findViewById(R.id.CalcGPA);
+        calcGPA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+
+
+                // get values from textviews
+                TextView gpa1 = findViewById(R.id.GPAInput1);
+                TextView gpa2 = findViewById(R.id.GPAInput2);
+                TextView gpa3 = findViewById(R.id.GPAInput3);
+                TextView gpa4 = findViewById(R.id.GPAInput4);
+                TextView gpa5 = findViewById(R.id.GPAInput5);
+
+
+                // convert textview values to ints
+                String strGPA1 = gpa1.getText().toString();
+                int intGPA1 = Integer.parseInt(strGPA1);
+                String strGPA2 = gpa2.getText().toString();
+                int intGPA2 = Integer.parseInt(strGPA2);
+                String strGPA3 = gpa3.getText().toString();
+                int intGPA3 = Integer.parseInt(strGPA3);
+                String strGPA4 = gpa4.getText().toString();
+                int intGPA4 = Integer.parseInt(strGPA4);
+                String strGPA5 = gpa5.getText().toString();
+                int intGPA5 = Integer.parseInt(strGPA5);
+
+
+                // calculate GPA
+                float GPA = (intGPA1 + intGPA2 + intGPA3 + intGPA4 + intGPA5) / 5;
+
+
+                TextView dspGPA = findViewById(R.id.DispGPA);
+                dspGPA.setText(Float.toString(GPA));
+
+                calcGPA.setText("clear form");
+
+                View background = findViewById(R.id.InputView);
+                View root = background.getRootView();
+
+                if(GPA < 61){
+                    root.setBackgroundColor(Color.RED);
+                }else if(GPA >= 61 && GPA < 80){
+                    root.setBackgroundColor(Color.YELLOW);
+                }else{
+                    root.setBackgroundColor(Color.GREEN);
+                }
+
+
+                // open new display with GPA
+                //openGPADisplay(GPA);
+
+            }
+        });
+
+
+
     }
 
-    public void calculate_gpa(View v)
-    {
-        TextView gpa1 = findViewById(R.id.GPAInput1);
-        TextView gpa2 = findViewById(R.id.GPAInput2);
-        TextView gpa3 = findViewById(R.id.GPAInput3);
-        TextView gpa4 = findViewById(R.id.GPAInput4);
-        TextView gpa5 = findViewById(R.id.GPAInput5);
-
-
-
-
-//        TextView tv = findViewById(R.id.msg);
-//        tv.setText("clicked");
+//    public void calculate_gpa(View v)
+//    {
+//        // get values from textviews
+//        TextView gpa1 = findViewById(R.id.GPAInput1);
+//        TextView gpa2 = findViewById(R.id.GPAInput2);
+//        TextView gpa3 = findViewById(R.id.GPAInput3);
+//        TextView gpa4 = findViewById(R.id.GPAInput4);
+//        TextView gpa5 = findViewById(R.id.GPAInput5);
 //
-//        if (counter%2 == 1)
-//        {
-//            tv.setText("counter is" +counter + "and it is odd");
-//        }else
-//        {
-//            tv.setText("counter is" +counter + "and it is even");
-//        }
+//        TextView GPA1View = findViewById(R.id.GPA1View);
+//        TextView GPA2View = findViewById(R.id.GPA2View);
+//        TextView GPA3View = findViewById(R.id.GPA3View);
+//        TextView GPA4View = findViewById(R.id.GPA4View);
+//        TextView GPA5View = findViewById(R.id.GPA5View);
 //
-//        counter++;
-    }
+//
+//        // convert textview values to floats
+//        float fltGPA1 = Float.valueOf(gpa1.getText().toString());
+//        float fltGPA2 = Float.valueOf(gpa2.getText().toString());
+//        float fltGPA3 = Float.valueOf(gpa3.getText().toString());
+//        float fltGPA4 = Float.valueOf(gpa4.getText().toString());
+//        float fltGPA5 = Float.valueOf(gpa5.getText().toString());
+//
+//        // calculate GPA
+//        float GPA = (fltGPA1 +fltGPA2 + fltGPA3 + fltGPA4 + fltGPA5) / 5;
+//
+//        gpa1.setVisibility(View.INVISIBLE);
+//        gpa2.setVisibility(View.INVISIBLE);
+//        gpa3.setVisibility(View.INVISIBLE);
+//        gpa4.setVisibility(View.INVISIBLE);
+//        gpa5.setVisibility(View.INVISIBLE);
+//
+//        GPA1View.setVisibility(View.INVISIBLE);
+//        GPA2View.setVisibility(View.INVISIBLE);
+//        GPA3View.setVisibility(View.INVISIBLE);
+//        GPA4View.setVisibility(View.INVISIBLE);
+//        GPA5View.setVisibility(View.INVISIBLE);
+//
+//        TextView dspGPA = findViewById(R.id.DispGPA);
+//        dspGPA.setVisibility(View.VISIBLE);
+//        dspGPA.setText(Float.toString(GPA));
+//
+//
+//        // open new display with GPA
+//        //openGPADisplay(GPA);
+//
+//    }
+
+
 }
