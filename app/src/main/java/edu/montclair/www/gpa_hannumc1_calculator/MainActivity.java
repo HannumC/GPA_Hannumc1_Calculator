64652327b5e74fe223818b1123909041eab5ed1f
Corey Hannum <coreyhannum@Coreys-MBP.fios-router.home>
2020-02-07 18:24:16 -0500

    new edits 2

--- app/src/main/java/edu/montclair/www/gpa_hannumc1_calculator/MainActivity.java
+++ app/src/main/java/edu/montclair/www/gpa_hannumc1_calculator/MainActivity.java
@@ -30,41 +30,60 @@ public class MainActivity extends AppCompatActivity {
                 TextView gpa4 = findViewById(R.id.GPAInput4);
                 TextView gpa5 = findViewById(R.id.GPAInput5);
 
+                gpa1.setBackgroundColor(Color.WHITE);
+                gpa2.setBackgroundColor(Color.WHITE);
+                gpa3.setBackgroundColor(Color.WHITE);
+                gpa4.setBackgroundColor(Color.WHITE);
+                gpa5.setBackgroundColor(Color.WHITE);
 
-                // convert textview values to ints
+
+                // convert textview values to string
                 String strGPA1 = gpa1.getText().toString();
-                int intGPA1 = Integer.parseInt(strGPA1);
                 String strGPA2 = gpa2.getText().toString();
-                int intGPA2 = Integer.parseInt(strGPA2);
                 String strGPA3 = gpa3.getText().toString();
-                int intGPA3 = Integer.parseInt(strGPA3);
                 String strGPA4 = gpa4.getText().toString();
-                int intGPA4 = Integer.parseInt(strGPA4);
                 String strGPA5 = gpa5.getText().toString();
-                int intGPA5 = Integer.parseInt(strGPA5);
 
 
-                // calculate GPA
-                float GPA = (intGPA1 + intGPA2 + intGPA3 + intGPA4 + intGPA5) / 5;
-
-
-                TextView dspGPA = findViewById(R.id.DispGPA);
-                dspGPA.setText(Float.toString(GPA));
-
-                calcGPA.setText("clear form");
-
-                View background = findViewById(R.id.InputView);
-                View root = background.getRootView();
-
-                if(GPA < 61){
-                    root.setBackgroundColor(Color.RED);
-                }else if(GPA >= 61 && GPA < 80){
-                    root.setBackgroundColor(Color.YELLOW);
+                if(strGPA1.matches("")) {
+                    gpa1.setBackgroundColor(Color.RED);
+                }else if(strGPA2.matches("")){
+                    gpa2.setBackgroundColor(Color.RED);
+                }else if(strGPA3.matches("")){
+                    gpa3.setBackgroundColor(Color.RED);
+                }else if(strGPA4.matches("")){
+                    gpa4.setBackgroundColor(Color.RED);
+                }else if(strGPA5.matches("")){
+                    gpa5.setBackgroundColor(Color.RED);
                 }else{
-                    root.setBackgroundColor(Color.GREEN);
+                    // convert textview values to int
+                    int intGPA1 = Integer.parseInt(strGPA1);
+                    int intGPA2 = Integer.parseInt(strGPA2);
+                    int intGPA3 = Integer.parseInt(strGPA3);
+                    int intGPA4 = Integer.parseInt(strGPA4);
+                    int intGPA5 = Integer.parseInt(strGPA5);
+
+                    // calculate GPA
+                    float GPA = (intGPA1 + intGPA2 + intGPA3 + intGPA4 + intGPA5) / 5;
+
+
+                    TextView dspGPA = findViewById(R.id.DispGPA);
+                    dspGPA.setText(Float.toString(GPA));
+
+                    calcGPA.setText("clear form");
+
+                    View background = findViewById(R.id.InputView);
+                    View root = background.getRootView();
+
+                    if (GPA < 61) {
+                        root.setBackgroundColor(Color.RED);
+                    } else if (GPA >= 61 && GPA < 80) {
+                        root.setBackgroundColor(Color.YELLOW);
+                    } else {
+                        root.setBackgroundColor(Color.GREEN);
+                    }
+
                 }
-
-
                 // open new display with GPA
                 //openGPADisplay(GPA);
 
